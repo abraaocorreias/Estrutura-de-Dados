379d213f7c18adc0957561935af0616870ea3112
abraaocorreias <abraaocorreias@gmail.com>
2026-03-08 04:57:18 -0300

    AULA 01

---
+++ Aula 01/aula 01.py
@@ -0,0 +1,11 @@
+#criar uma lista
+lista_de_compras = ["leite", "pão", "ovos", "frutas"]   
+
+#adicionar itens a lista
+lista_de_compras.append("queijo")
+lista_de_compras.append("manteiga")
+lista_de_compras.append("café")
+
+#remover itens da lista
+lista_de_compras.remove("leite")
+lista_de_compras.remove("pão")
